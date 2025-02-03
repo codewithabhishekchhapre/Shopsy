789829938eb2a20e74c4c997d8197aee0baf743a
abhishek chhapre <abhishekgurjar5020@gmail.com>
2025-02-03 14:12:00 +0530

    Initial commit

---
+++ Shopsyenv/Shopsyproject/Shopsy/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+
+def Homepage(request):
+     template = loader.get_template('index.html')
+     return HttpResponse(template.render())
+
+def Hello(request):
+    return HttpResponse("Hello world!")
